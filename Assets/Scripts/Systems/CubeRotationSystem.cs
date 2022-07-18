349df66b29af5788555a7787627961fca83a2852
Jonas00016 <juergens.jonas2000@gmail.com>
2022-07-18 17:40:40 +0200

    created rotation script

---
+++ Assets/Scripts/Systems/CubeRotationSystem.cs
@@ -0,0 +1,20 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public partial class CubeRotationSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.WithAll<CubeTag>().ForEach((ref Rotation rotation) => {
+            rotation.Value = math.mul(rotation.Value, quaternion.RotateX(deltaTime));
+            rotation.Value = math.mul(rotation.Value, quaternion.RotateY(deltaTime));
+            rotation.Value = math.mul(rotation.Value, quaternion.RotateZ(deltaTime));
+        }).ScheduleParallel();
+    }
+}
