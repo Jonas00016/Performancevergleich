6cc9c0d6e9d439a8f4f29a4eecaaf942b6112d8d
Jonas00016 <juergens.jonas2000@gmail.com>
2022-07-20 16:33:09 +0200

    player movement working with physics

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -3,6 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -10,21 +11,19 @@ public partial class PlayerMovementSystem : SystemBase
 {
     private float3 FORWARD = new float3(0f, 0f, 1f);
     private float3 SIDEWAYS = new float3(1f, 0f, 0f);
-    private const float MOVEMENTSPEED = 5;
+    private const float MOVEMENTSPEED = 500;
 
     [BurstCompile]
     protected override void OnUpdate()
     {
-        float3 vertical = FORWARD * Input.GetAxis("Vertical");
-        float3 horizontal = SIDEWAYS * Input.GetAxis("Horizontal");
-
-        if (vertical.Equals(horizontal)) return;
+        float3 vertical = FORWARD * (Input.GetKey("w") ? 1f : (Input.GetKey("s") ? -1f : 0f ));
+        float3 horizontal = SIDEWAYS * (Input.GetKey("d") ? 1f : (Input.GetKey("a") ? -1f : 0f));
 
         float deltaTime = Time.DeltaTime;
         float speed = MOVEMENTSPEED;
 
-        Entities.WithAll<PlayerTag>().ForEach((ref Translation translation, in Rotation rotation) => {
-            translation.Value += math.mul(rotation.Value, math.normalize(vertical + horizontal) * speed * deltaTime);
+        Entities.WithAll<PlayerTag>().ForEach((ref PhysicsVelocity velocity, in Rotation rotation) => {
+            velocity.Linear = math.mul(rotation.Value, math.normalizesafe(vertical + horizontal, 0f) * speed * deltaTime);
         }).Schedule();
     }
 }
