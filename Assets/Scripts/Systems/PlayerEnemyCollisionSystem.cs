0f101f531cc3a9ef53ec06f097600f49a32cccbc
Jonas00016 <juergens.jonas2000@gmail.com>
2022-07-22 17:44:41 +0200

    wip collision

--- Assets/Scripts/Systems/PlayerEnemyCollisionSystem.cs
+++ Assets/Scripts/Systems/PlayerEnemyCollisionSystem.cs
@@ -11,8 +11,46 @@ using Unity.Physics.Systems;
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 public partial class PlayerEnemyCollisionSystem : SystemBase
 {
+    private StepPhysicsWorld stepPhysicsWorld;
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private EndFixedStepSimulationEntityCommandBufferSystem endFixedStepSimulationECB;
+
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<PlayerTag>();
+        RequireSingletonForUpdate<EnemyTag>();
+
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        endFixedStepSimulationECB = World.GetOrCreateSystem<EndFixedStepSimulationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
+        EntityCommandBuffer commandBuffer = endFixedStepSimulationECB.CreateCommandBuffer();
 
+        JobHandle collisionEventHandle = new CollisionJob
+        {
+           /* players = GetComponentDataFromEntity<PlayerTag>(),
+            enemies = GetComponentDataFromEntity<EnemyTag>(),
+            ecb = commandBuffer*/
+        }.Schedule(stepPhysicsWorld.Simulation, Dependency);
+
+        collisionEventHandle.Complete();
+
+        endFixedStepSimulationECB.AddJobHandleForProducer(Dependency);
+    }
+}
+
+public struct CollisionJob : ICollisionEventsJob
+{
+    /*public ComponentDataFromEntity<PlayerTag> players;
+    public ComponentDataFromEntity<EnemyTag> enemies;
+    public EntityCommandBuffer ecb;*/
+
+    public void Execute(CollisionEvent collisionEvent)
+    {
+        Debug.Log("A: " + collisionEvent.EntityA);
+        Debug.Log("B: " + collisionEvent.EntityB);
     }
 }
